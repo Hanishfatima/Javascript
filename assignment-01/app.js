52578928145e651df183b50ccc75375b1b144a7d
Hanishfatima <hanishfatimaa@gmail.com>
2025-09-02 02:09:19 +0500

    completed

--- assignment-01/app.js
+++ assignment-01/app.js
@@ -1,8 +1,13 @@
+// *** /
+
 // 1.
 // Write a script to greet your website visitor using JS alert
 //box.
 
 alert("Welcome to my website"); 
+
+
+// *** /
 
 // 2. 
 // Write a script to display following message on your web
@@ -11,6 +16,9 @@ alert("Welcome to my website");
 //ANSWER
 //alert("Hello welcome to my webpage")
 
+
+// *** /
+
 // 3.
 //  Write a script to display following message on your web
 //page: (Hint : Use line break)
@@ -18,9 +26,34 @@ alert("Welcome to my website");
 //ANSWER
 //alert("Welcome to JS Land \nHappy coding!");
 
+
+// *** /
+
 //4.
 //  Write a script to display following messages in sequence:
 
 //ANSWER
 
-//alert("Welcome to JS land")
+//alert("Welcome to JS land...")
+
+//alert("Happy coding! \nPrevent this page from creating additional dialogs");
+
+// *** /
+
+
+//5.
+//  Generate the following message through browser’s
+//developer console:
+
+//ANSWER
+//alert("Hello... I can run JS through my web browser's console")
+
+// *** /
+
+//6.
+//Make use of alerts in your new/existing HTML & CSS
+//project.
+
+// <a href="https://github.com/Hanishfatima/class-work-"></a>
+//    
+    

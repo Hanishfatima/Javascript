055887dfa83d9df6a42a985ca2a0dfb3ddc36811
Hanishfatima <hanishfatimaa@gmail.com>
2025-09-02 00:10:52 +0500

    starting

---
+++ assignment-01/app.js
@@ -0,0 +1,26 @@
+// 1.
+// Write a script to greet your website visitor using JS alert
+//box.
+
+alert("Welcome to my website"); 
+
+// 2. 
+// Write a script to display following message on your web
+//page:
+
+//ANSWER
+//alert("Hello welcome to my webpage")
+
+// 3.
+//  Write a script to display following message on your web
+//page: (Hint : Use line break)
+
+//ANSWER
+//alert("Welcome to JS Land \nHappy coding!");
+
+//4.
+//  Write a script to display following messages in sequence:
+
+//ANSWER
+
+//alert("Welcome to JS land")

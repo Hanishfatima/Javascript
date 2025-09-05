c7c8b343795af51ab2cce7d0e104c8263708a3b9
Hanishfatima <hanishfatimaa@gmail.com>
2025-09-05 20:24:25 +0500

    pending

---
+++ assignment-04/app.js
@@ -0,0 +1,28 @@
+//Question # 1
+//
+//1. Declare 3 variables in one statement.
+//
+//let firstName = "Hanish" , lastName = "Fatima" , fatherName = "Amirbaig " ;
+//console.log( firstName , lastName  , fatherName)
+//
+//
+//Question # 2
+//2. Declare 5 legal & 5 illegal variable names.
+//
+//*5 LEGAL VARIABLE NAMES*
+
+//let userName ;
+//let user123 ;
+//let $amount ;
+//let student_Name ;
+//let _firstName ;
+
+
+//*5 ILLEGAL VARIABLE NAMES*
+
+//let Firstname ;
+//let first name ;
+//let FIRSTname ;
+//let @amount ;
+//let first-Name;
+

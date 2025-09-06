42b2975e2247ec5aade788146fd9219b0af04d54
Hanishfatima <hanishfatimaa@gmail.com>
2025-09-07 02:47:37 +0500

    completed

--- assignment-04/app.js
+++ assignment-04/app.js
@@ -1,28 +1,56 @@
-//Question # 1
-//
-//1. Declare 3 variables in one statement.
-//
-//let firstName = "Hanish" , lastName = "Fatima" , fatherName = "Amirbaig " ;
-//console.log( firstName , lastName  , fatherName)
-//
-//
-//Question # 2
-//2. Declare 5 legal & 5 illegal variable names.
-//
-//*5 LEGAL VARIABLE NAMES*
+/* Question # 1
 
-//let userName ;
-//let user123 ;
-//let $amount ;
-//let student_Name ;
-//let _firstName ;
+1. Declare 3 variables in one statement.
+
+let firstName = "Hanish" , lastName = "Fatima" , fatherName = "Amirbaig " ;
+console.log( firstName , lastName  , fatherName)
 
 
-//*5 ILLEGAL VARIABLE NAMES*
+Question # 2
+2. Declare 5 legal & 5 illegal variable names.
 
-//let Firstname ;
-//let first name ;
-//let FIRSTname ;
-//let @amount ;
-//let first-Name;
+   *5 LEGAL VARIABLE NAMES*
 
+       let userName ;
+       let user123 ;
+       let $amount ;
+       let student_Name ;
+       let _firstName ;
+
+
+   *5 ILLEGAL VARIABLE NAMES*
+
+       let Firstname ;
+       let first name ;
+       let FIRSTname ;
+       let @amount ;
+       let first-Name;
+
+Question # 3
+
+3. Display this in your browser
+
+a) A heading stating “Rules for naming JS variables”
+b) Variable names can only contain ______, ______,
+______ and ______.
+
+“Rules for naming JS variables”
+
+1: Letters
+2: Numbers
+3: Underscore(_)
+4: Dollar sign ($)
+
+c) Variables must begin with a ______, ______ or
+_____. For example $name, _name or name
+
+1: name
+2: $name
+3: _name
+
+d) Variable names are case _________
+
+* Sensitive
+
+e) Variable names should not be JS _________
+* JS Keywords */

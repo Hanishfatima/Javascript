da6718134d75b919d82fe818722f30c80e7cbbf3
Hanishfatima <hanishfatimaa@gmail.com>
2025-09-10 16:19:08 +0500

    pending

---
+++ assignment-05/app.js
@@ -0,0 +1,62 @@
+//Question # 1
+
+//1. Write a program that take two numbers & add them in a
+//new variable. Show the result in your browser.
+
+//let num1 = 3
+//let num2 = 5
+//let sum  = (num1 + num2 )
+//document.write(`Sum of  ${num1}  and ${num2} is ${sum}`);
+
+//Question # 2
+
+//2. Repeat task1 for subtraction, multiplication, division &
+//modulus.
+
+
+//let number1 = 3
+//let number2 = 5
+//let subtract  = (num1 - num2 )
+//document.write(`Subtraction of  ${number1}  and ${number2} is ${subtract}`);
+
+//let dataNum1 = 3
+//let  dataNum2 = 5
+//let multiplication  = ( dataNum1 * dataNum2 )
+//document.write(`Multiplication of  ${dataNum1}  and ${dataNum2} is ${multiplication}`);
+
+//let data1= 3
+//let  data2 = 5
+//let division  = ( data1 / data2 )
+//document.write(`Division of  ${data1}  and ${data2} is ${division}`);
+
+//let value1= 3
+//let  value2 = 5
+//let modulus  = ( value1 % value2 )
+//document.write(`Modulus of  ${value1}  and ${value2} is ${modulus}`);
+
+//Question # 4
+
+//4. Cost of one movie ticket is 600 PKR. Write a script to
+//store
+//ticket price in a variable & calculate the cost of buying 5
+//tickets
+//to a movie. Example output:
+
+//let ticket= 600
+//let  quantity = 5
+//let  totalCost  = (  ticket * quantity  )
+//document.write(`Total cost to buy 5 tickets to a movie is ${totalCost}PKR`);
+
+//Question # 5
+
+let num1 = 4
+let one = 1
+let multiply = 4 * 1 
+
+let num2 = 4
+let two = 2
+let multiply2 = 4 * 1 
+
+document.write( `${num1} x ${one} = ${multiply}
+    
+    ${num2} x ${two} = ${multiply2}`)

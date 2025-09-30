f7d75d53f80b93f3b0f1540b378c77d62bd3de0b
Hanishfatima <hanishfatimaa@gmail.com>
2025-09-30 17:03:48 +0500

    assignment 10 pending

---
+++ assignment-10/app.js
@@ -0,0 +1,36 @@
+//QUESTION 01
+
+//let arr = [[],[]]
+
+//QUESTION 02
+
+//let arr = [
+//    [0,1,2,3],
+//    [1,0,1,2],
+//    [2,1,0,1]
+//]
+//for (let i = 0 ; i < arr.length ; i++){
+//document.write(arr[i] + "<br />" )
+//}
+
+//QUESTION 03
+
+//for (let i = 1 ; i <= 10 ; i++ ){
+//    document.write(i + "<br />" )
+//}
+
+//QUESTION 04
+
+//prnquoiiie
+
+//QUESTION 05
+
+let fruits = ["apple", "mango", "banana", "orange", "strawberry"]
+for (let i = 0 ; i < fruits.length ; i++){
+    document.write(fruits[i] + "<br />")
+}
+document.write (`Element at index ${fruits.length-5} is ${fruits[fruits.length-5]}`,
+    `Element at index ${fruits.length-4} is ${fruits[fruits.length-4]}`,
+    `Element at index ${fruits.length-3} is ${fruits[fruits.length-3]}`
+)
+
